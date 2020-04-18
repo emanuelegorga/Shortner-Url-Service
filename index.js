1e29a7b385e847a7b44f14c789ac87afe2e8c4bd
Emanuele Gorga <emanuelegorga@gmail.com>
2020-04-18 13:09:52 +0200

    Create the route to manage the action of shortening an URL: It takes the url from the client, the base URL (which would be the localhost), create an urlCode which would be a random string to append after the base Url, create an Url object passing the original long url, the short url, the url code and the date, save it to the db and returning. If anything during the process goes wrong will return 500. If the long url coming from the client is invalid returns 401.

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
+// Define routes
+
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/index'));
+
 const PORT = 5000;
 
 app.listen(PORT, () => {
